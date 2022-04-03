add524252503d0491eda132e1754b33f1d16cd17
francescodeaglio <francesco.deaglio@gmail.com>
2022-04-03 17:51:48 +0200

    PCA chart

--- streamlit_home.py
+++ streamlit_home.py
@@ -2,10 +2,11 @@ import streamlit as st
 from intro import intro
 from pattern_finder import pattern_finder
 from cypher_box import cypher_box
+from pcapage import pca_page
 
 if __name__ == '__main__':
     st.sidebar.title("Cy4soccer")
-    page = st.sidebar.selectbox("Select the page", ["Intro", "Pattern Finder", "Cypher", "Credits"])
+    page = st.sidebar.selectbox("Select the page", ["Intro", "Pattern Finder", "Cypher", "PCA", "Credits"])
 
     if page == "Intro":
         intro()
@@ -18,3 +19,6 @@ if __name__ == '__main__':
 
     if page == "Credits":
         st.write("scrivi due cazzate")
+
+    if page == "PCA":
+        pca_page()
